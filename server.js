9b3c08d7f085b83ff85ac21fca737b28342959da
trvandat02 <trvandat02@gmail.com>
2024-03-08 08:20:06 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// start server
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the datebase!");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}.`);
-});
+        const PORT = config.app.port;
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}.`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
+
+//Start server
+
+// const PORT = config.app.port;
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on port: ${PORT}.`);
+// });
+
+
